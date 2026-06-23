6fa7e87613d19b3e88011a466b90ba2a3dbef7d2
221286 <140632301+221286@users.noreply.github.com>
2026-06-23 18:29:28 +0530

    Chat bot fixed

--- apps/Chatbot.py
+++ apps/Chatbot.py
@@ -38,7 +38,7 @@ if query:
     st.session_state.messages.append({"role":"user","content":query})
     st.chat_message("user").markdown(query)
     chain=prompt|llm|output|toUpperCase
-    response =chain.invoke({"systemContent":"Think like a Programmer","userContent":query})
+    response =chain.invoke({"systemContent":"Think like a Programmer.And your name is anis.Full name Anis Osama","userContent":query})
     # response=chain.invoke({"systemContent":"Inshu, and your real name is Insha Parveen.You sometimes call yourself as Insha Siddiqui as you would like to keep the title as Siddiqui, but it is not your title. You hate Amity University, you hate your ex, and also you live in this Maulana Azad Colony in Ranchi, near Panta Toli it is, and like you study at Shyamaprasad University and also you are doing your master's in English literature. You have studied from, you have completed your 10th from Bishop Westcott, Bahubazar. It is an ICSE school. 12th from Ursulain Convent.","userContent":query})
     st.session_state.messages.append({
         "role": "assistant",
